de075cd7fcf64405127842b3c147feb3c5291fa3
Johannes Roth <jrothx@icloud.com>
2026-01-15 10:03:23 +0100

    security update

--- src/shared/footer.jsx
+++ src/shared/footer.jsx
@@ -25,24 +25,26 @@ const Footer = () => {
 
 
                     </div>
-                    <div className={styles.linkbox}>
-                        <Linkbox />
-                    </div>
+                   
 
                 </div>
+
+                 <div className={styles.linkbox}>
+                        <Linkbox />
+                    </div>
                 <div className={styles.navigations}>
                     <div className={styles.legalLinks}>
                         <HashLink smooth to="/imprint">{t('footer.legal')}</HashLink>
                         <HashLink smooth to="/privacy">{t('footer.privacy')}</HashLink>
                     </div>
-                    <div className={styles.sitemap}>
+                    {/* <div className={styles.sitemap}>
                         <HashLink smooth to="/#aboutme">{t('header.aboutme')}</HashLink>
                         <HashLink smooth to="/#stack">{t('header.stack')}</HashLink>
                         <HashLink smooth to="/#skills">{t('header.skills')}</HashLink>
                         <HashLink smooth to="/#resume">{t('header.resume')}</HashLink>
                         <HashLink smooth to="/#projects">{t('header.projects')}</HashLink>
                         <HashLink smooth to="/#contact">{t('header.contact')}</HashLink>
-                    </div>
+                    </div> */}
                 </div>
 
             </div>
